b2e502104c19e31527c582ec9a051bf5635ea7ae
Alexandre Burlot <burlotalexandre@yahoo.fr>
2020-12-15 08:27:28 +0100

    création

---
+++ Main.py
@@ -0,0 +1,5 @@
+# BURLOT Alexandre, SIBENALER Arnaud
+# début du projet le 15/12/2020
+# Objectif : réalisation d'un space invader sous Tkinter
+# TODO : 
+
---
+++ myLib.py
@@ -0,0 +1,5 @@
+# BURLOT Alexandre, SIBENALER Arnaud
+# début du projet le 15/12/2020
+# Objectif : bibliothèque de fonctions pour le space invader
+# TODO :
+
